cf9ed237f371367dd4bd01bf456bbf822dc41df6
student <student@localhost>
2019-01-15 18:29:38 +0400

    Fixed #1 Fixed #2 Fixed #3 Fixed #4 Fixed #5 Fixed #6

--- src/main/java/ru/itpark/DepositService.java
+++ src/main/java/ru/itpark/DepositService.java
@@ -4,7 +4,7 @@ public class DepositService {
     public int depositCalculator(int depositAmount, int depositDataDays, int interestRate) {
         int allDaysYear = 365;
 
-        int depositWithInterest = (depositAmount + (interestRate * depositDataDays * depositAmount / 100 / allDaysYear));
+        int depositWithInterest = depositAmount + interestRate * depositDataDays * depositAmount / 100 / allDaysYear;
 
         return depositWithInterest;
     }
--- src/test/java/ru/itpark/DepositServiceTest.java
+++ src/test/java/ru/itpark/DepositServiceTest.java
@@ -1,12 +1,53 @@
 package ru.itpark;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepositServiceTest {
-
     @Test
+    @DisplayName("Deposit Amount 100_000, deposit data days 63, interest rate 10")
+    void depositCalculatorCaseFirst() {
+        {
+            DepositService service = new DepositService();
+            int depositCalculator = service.depositCalculator(100_000,63,10);
+
+            assertEquals(101_726,depositCalculator);
+        }
+    }
+    @Test
+    @DisplayName("Deposit Amount 100_000, deposit data days 63, interest rate 0")
+    void depositCalculatorCaseSecond() {
+        {
+            DepositService service = new DepositService();
+            int depositCalculator = service.depositCalculator(100_000,63,0);
+
+            assertEquals(100_000,depositCalculator);
+        }
+    }
+    @Test
+    @DisplayName("Deposit Amount 100_000, deposit data days 0, interest rate 10")
+    void depositCalculatorCaseThird() {
+        {
+            DepositService service = new DepositService();
+            int depositCalculator = service.depositCalculator(100_000,0,10);
+
+            assertEquals(100_000,depositCalculator);
+        }
+    }
+    @Test
+    @DisplayName("Deposit Amount 100_000, deposit data days 0, interest rate 10")
+    void depositCalculatorCasefourth() {
+        {
+            DepositService service = new DepositService();
+            int depositCalculator = service.depositCalculator(100_000,20,10);
+
+            assertEquals(100_547,depositCalculator);
+        }
+    }
+    @Test
+    @DisplayName("Deposit Amount 0")
     void depositCalculator() {
         {
             DepositService service = new DepositService();
@@ -14,30 +55,5 @@ class DepositServiceTest {
 
             assertEquals(0,depositCalculator);
         }
-        {
-            DepositService service = new DepositService();
-            int depositCalculator = service.depositCalculator(100_000,63,10);
-
-            assertEquals(101_726,depositCalculator);
-        }
-        {
-            DepositService service = new DepositService();
-            int depositCalculator = service.depositCalculator(100_000,63,0);
-
-            assertEquals(100_000,depositCalculator);
-        }
-        {
-            DepositService service = new DepositService();
-            int depositCalculator = service.depositCalculator(100_000,0,10);
-
-            assertEquals(100_000,depositCalculator);
-        }
-        {
-            DepositService service = new DepositService();
-            int depositCalculator = service.depositCalculator(100_000,20,10);
-
-            assertEquals(100_547,depositCalculator);
-        }
-
     }
 }
